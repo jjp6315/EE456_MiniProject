0a0f845e4ec8a845728d6c47d67e029c24b8c1f6
John Park <jjp6315@psu.edu>
2023-11-09 13:08:47 -0500

    file to see what the data looks like

--- Backpropagation.m
+++ Backpropagation.m
@@ -1,3 +1,17 @@
 clear;
 clc;
 close all;
+
+numInputLayer = 2;
+numHiddenLayer = 20;
+numOutputLayer = 1;
+
+% Activation Function is a hyperbolic tangent function
+% Threshold = 0
+
+learningRate = 0.1;
+
+load('DataSet1_MP1.mat');
+
+
+s
---
+++ DisplayData.m
@@ -0,0 +1,10 @@
+load('DataSet1_MP1.mat');
+
+
+% Scatter plot
+scatter(DataSet1(:, 1), DataSet1(:, 2), 10, DataSet1_targets);
+
+
+% Add labels and title
+title('Scatter Plot of DataSet1');
+
