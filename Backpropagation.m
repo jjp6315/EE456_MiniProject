d95e60a0fd18e45e46e2fadb7bb7502f9ca7a773
John Park <jjp6315@psu.edu>
2023-11-09 21:33:43 -0500

    I think done steps 1-5

--- Backpropagation.m
+++ Backpropagation.m
@@ -23,7 +23,7 @@ weights_hidden_output = randn(numHiddenLayer, numOutputLayer);
 
 
 biases_hidden = -ones(numHiddenLayer, 1);
-biases_output = -ones(numHiddenLayer, 1);
+biases_output = -ones(numOutputLayer, 1);
 
 flag = 1;
 
@@ -38,23 +38,40 @@ flag = 1;
 
 % The last criterion is testing for generalization performance after each epoch. 
 
-hidden_layer_input = zeroes(numHiddenLayer, 1);
+hidden_layer_input = zeros(numHiddenLayer, 1);
 
 
 for x = 1:numHiddenLayer
-    for i = 1:numInputLayer
-        hidden_layer_input(x, 1) = hidden_layer_input + 
+    for i = 1:numData
+        hidden_layer_input(x, 1) = hidden_layer_input(x, 1) + DataSet1(i, 1) * weights_input_hidden(1, x) + DataSet1(i, 2) * weights_input_hidden(2, x);
     end
+    hidden_layer_input(x, 1) = hidden_layer_input(x, 1) + biases_hidden(x, 1);
 end
+hidden_layer_output = tanh(hidden_layer_input);
 
-while flag
-    hidden_layer_input = DataSet1 * weights_input_hidden + biases_hidden;
-    hidden_layer_output = tanh(hidden_layer_input);
-    
-    output_layer_input = hidden_layer_output * weights_hidden_output;
-    predicted_output = tanh(output_layer_input);
-    flag = 0;
+
+output_layer = zeros(numOutputLayer, 1);
+
+for y = 1:numHiddenLayer
+    output_layer(1, 1) = output_layer(1, 1) + hidden_layer_output(y, 1) * weights_hidden_output(y, 1);
 end
+output_layer(1, 1) = output_layer(1, 1) + biases_output(1, 1);
+
+class_out = tanh(output_layer);
+
+% disp(class_out);
+% disp(DataSet1_targets(1));
+
+% disp(hidden_layer_output);
+
+% while flag
+%     hidden_layer_input = DataSet1 * weights_input_hidden + biases_hidden;
+%     hidden_layer_output = tanh(hidden_layer_input);
+% 
+%     output_layer_input = hidden_layer_output * weights_hidden_output;
+%     predicted_output = tanh(output_layer_input);
+%     flag = 0;
+% end
 
 % disp(hidden_layer_input);
 % disp(hidden_layer_output);
