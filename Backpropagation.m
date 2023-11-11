2a6f2665aacd47643b03cdd01b9299ad393afa62
Liao <czl5899@psu.edu>
2023-11-10 22:45:54 -0500

    adding training and val steps in graphs

--- Backpropagation.m
+++ Backpropagation.m
@@ -4,6 +4,27 @@ close all;
 
 load('DataSet1_MP1.mat');
 
+% handle the data
+inputClass1 = DataSet1(1:3000, :);
+inputClass2 = DataSet1(3001:6000, :);
+targetClass1 = DataSet1_targets(1:3000);
+targetClass2 = DataSet1_targets(3001:6000);
+
+inputTrain = zeros(4800, 2);
+inputVal = zeros(1200, 2);
+targetTrain = zeros(4800);
+targetVal = zeros(1200);
+
+inputTrain(1:2400, :) = inputClass1(1:2400, :);
+inputTrain(2401:4800, :) = inputClass2(1:2400, :);
+targetTrain(1:2400) = targetClass1(1:2400, :);
+targetTrain(2401:4800) = targetClass2(1:2400, :);
+
+inputVal(1:600, :) = inputClass1(2401:3000, :);
+inputVal(601:1200, :) = inputClass2(2401:3000, :);
+targetVal(1:600) = targetClass1(2401:3000, :);
+targetVal(601:1200) = targetClass2(2401:3000, :);
+
 % properties of the layers
 numInputNeurons = 2;
 numHiddenNeurons = 20;
@@ -11,7 +32,7 @@ numOutputNeurons = 1;
 
 % properties of the NN
 learningRate = 0.1;
-epochs = 1000;
+epochs = 50;
 
 % annealed linearly from 10^-1 down to 10^-5
 annealRate = (0.1-0.00001)/epochs;
@@ -22,16 +43,19 @@ b1 = randn(numHiddenNeurons, 1);
 w2 = randn(numOutputNeurons, numHiddenNeurons);
 b2 = randn(numOutputNeurons, 1);
 
-total_errors = zeros(epochs);
-iteration = zeros(epochs);
+total_errors_train = zeros(epochs);
+iteration_train = zeros(epochs);
+
+total_errors_val = zeros(epochs/10);
+iteration_val = zeros(epochs/10);
 
 % start the training
 for epoch = 1:epochs
-    error = 0;
-    for index = 1:6000
+    error_train = 0;
+    for index = 1:4800
         % getting the input and target
-        x = DataSet1(epoch, :);
-        y = DataSet1_targets(epoch);
+        x = inputTrain(epoch, :);
+        y = targetTrain(epoch);
     
         % forward pass
         % input layer to hidden layer (z_j => 20x1)
@@ -64,19 +88,72 @@ for epoch = 1:epochs
         b1 = b1 + change_alpha_0j; % 20x1
     
         % keep track of the losses
-        error = error + (y_k-y)^2;
+        error_train = error_train + (y_k-y)^2;
     end
+
+    % do validation
+    if mod(epoch, 10) == 0
+        error_val = 0;
+        for index = 1:1200
+            % getting the input and target
+            x = inputVal(epoch, :);
+            y = targetVal(epoch);
+        
+            % forward pass
+            % input layer to hidden layer (z_j => 20x1)
+            z_in_j = (w1 * x') + b1; % 20x1
+            z_j = tanh(z_in_j); % 20x1
+            % hidden layer to output layer (a2 => 1x1)
+            y_in_k = w2 * z_j + b2; % 1x1
+            y_k = tanh(y_in_k); % 1x1
+    
+            % back pass
+            % output to hidden layer (delta_k => 20x1)
+            delta_k = (y - y_k) * der_tanh(y_in_k); % 1x1
+            change_w_jk = learningRate * (delta_k .* z_j); % 20x1
+            change_w_0k = learningRate * delta_k; %1x1
+            % hidden to input layer (delta_j => 20x1)
+            delta_in_j = delta_k .* w2'; % 20x1
+            delta_j = delta_in_j .* der_tanh(z_in_j); % 20x1
+            change_alpha_ij = zeros(20, 2); % 20x2
+            for row = 1:20
+                for column = 1:2
+                    change_alpha_ij(row, column) = learningRate * delta_j(row) * x(column);
+                end
+            end
+            change_alpha_0j = learningRate .* delta_j; % 20x1
+        
+            % update the weights and bias
+            w2 = w2 + change_w_jk'; % 1x20
+            b2 = b2 + change_w_0k; % 1x1
+            w1 = w1 + change_alpha_ij; % 20x2
+            b1 = b1 + change_alpha_0j; % 20x1
+        
+            % keep track of the losses
+            error_val = error_val + (y_k-y)^2;
+        end
+        total_errors_val(epoch/10) = error_train/1200;
+        iteration_val(epoch/10) = epoch;
+    end
+
     % update the learning rate
     learningRate = learningRate - annealRate;
     disp(w2);
-    total_errors(epoch) = error/6000;
-    iteration(epoch) = epoch;
+    total_errors_train(epoch) = error_train/4800;
+    iteration_train(epoch) = epoch;
 end
 
 % plotting the losses
-figure;
-plot(iteration, total_errors);
-title('Error Plot over Epochs');
+figure(1);
+plot(iteration_train, total_errors_train);
+title('Error Plot over Epochs(train)');
+xlabel('Epochs');
+ylabel('Error');
+grid on;
+
+figure(2);
+plot(iteration_val, total_errors_val);
+title('Error Plot over Epochs (val)');
 xlabel('Epochs');
 ylabel('Error');
 grid on;
